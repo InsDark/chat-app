a2372a395d11b37d95cf3afa09891f4afac1b500
Chrono <matoshurtadodiegoaquiles@gmail.com>
2023-02-14 21:47:45 -0500

    last adjustments

--- src/components/ChatContainer.jsx
+++ src/components/ChatContainer.jsx
@@ -14,12 +14,12 @@ const ChatContainer = ({}) => {
     <div className=' p-2 overflow-y-auto h-full gap-2 bg-slate-600 flex flex-col' ref={chatDiv}>
           {messages ? messages.map((msg, index) =>
           (
-            <div key={index} className={ msg.picture == auth.user.photoURL ? 'bg-slate-800 w-fit  self-end p-2 rounded' : 'bg-slate-800 w-fit p-2 rounded '}>
-              <div className='flex'>
+            <div key={index} className={ msg.picture == auth.user.photoURL ? 'bg-slate-800 w-fit  self-end rounded' : 'bg-slate-800 w-fit  rounded '}>
+              <div className='flex gap-2 items-center bg-slate-700 p-2'>
                 <img className='w-12 h-12' src={msg.picture} alt={msg.picture} />
                 <h2>{msg.user}</h2>
               </div>
-              <p>{msg.message}</p>
+              <p className='p-2'>{msg.message}</p>
             </div>
           )
           ) : <h3>no messages</h3>}
